9d1e040560c730054a2fb24ed553b50a82ee0b85
gohyixuan <gohyixuan02@gmail.com>
2023-02-17 17:07:36 +0800

    Consume new methods in main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,13 +16,16 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        printData(tasksData);
-        System.out.println();
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+//        System.out.println();
+//        System.out.println("Printing deadlines");
+//        printDeadlines(tasksData);
+//
+//
+//        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+//        printData(tasksData);
+//        printDataUsingStream(tasksData);
+        printDeadlinesUsingStream(tasksData);
+        countDeadlinesUsingStream(tasksData);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
